ee4b9eedd9dd1ef5c8f92dcbc4ef2d1a75e11398
Matheus_Fsilva <matheus.francisco20021@gmail.com>
2025-11-14 18:13:56 -0300

    Otimizando o codigo

--- app.ts
+++ app.ts
@@ -17,10 +17,16 @@ import path from "path";
 
 const app = express();
 app.use(cors({
-    origin: ["https://rma-controller.vercel.app"],
+    origin: [
+        "https://rma-controller.vercel.app",
+        "http://localhost:3000"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-}));  
+    credentials: true
+}));
+
+app.options("*", cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
