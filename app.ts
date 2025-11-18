c9f60fad87f667d86f4b41c4d07c264a65af2288
Matheus_Fsilva <matheus.francisco20021@gmail.com>
2025-11-18 17:04:52 -0300

    rota automatica

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors"
-import cron from "node-cron";
+
 import { ReturnModel } from "./models/returnModel";
 import { ShopModel } from "./models/shopModel";
 import { FinishModel } from "./models/finishModel";
@@ -11,7 +11,7 @@ import returnRoutes from "./router/Returns"
 import tokenRoutes from "./router/Tokens"
 import shopRoutes from "./router/Shop"
 
-import SeachReturns from "./utils/returns/SeachReturns";
+
 
 import path from "path";
 
