bc17d0d8dd5e29abe6ccdcac9932509bf355006c
Matheus_Fsilva <matheus.francisco20021@gmail.com>
2025-12-22 17:21:02 -0300

    webhooks v2

--- dist/router/Webhooks.js
+++ dist/router/Webhooks.js
@@ -8,6 +8,14 @@ const router = express_1.default.Router();
 router.post("/shopee", (req, res) => {
     console.log("📩 CHEGOU DA SHOPEE");
     console.log(req.body);
+    // 🔐 mensagem de verificação
+    if (req.body.code === 0) {
+        return res.status(200).json({
+            code: 0,
+            message: "success"
+        });
+    }
+    // eventos normais
     res.status(200).json({ ok: true });
 });
 exports.default = router;
--- router/Webhooks.ts
+++ router/Webhooks.ts
@@ -6,7 +6,17 @@ router.post("/shopee", (req, res) => {
   console.log("📩 CHEGOU DA SHOPEE");
   console.log(req.body);
 
+  // 🔐 mensagem de verificação
+  if (req.body.code === 0) {
+    return res.status(200).json({
+      code: 0,
+      message: "success"
+    });
+  }
+
+  // eventos normais
   res.status(200).json({ ok: true });
 });
+
 
 export default router;
